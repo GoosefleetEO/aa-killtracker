4ca355f7eb24c731c11a817b1265df04770c2181
ErikKalkoken <erik.kalkoken@gmail.com>
2020-05-17 18:48:00 +0200

    Move logic to manager

--- killtracker/managers.py
+++ killtracker/managers.py
@@ -1,13 +1,54 @@
 import logging
-from django.db import models
+from time import sleep
+
+import requests
+
+from django.db import models, transaction
+
 from esi.models import esi_client_factory
 
 
 _client = None
 logger = logging.getLogger(__name__)
 
+ZKB_REDISQ_URL = 'https://redisq.zkillboard.com/listen.php'
+ZKB_REDISQ_TIMEOUT = 5
+ESI_API_TIMEOUT = 5
+
+CHARACTER_PROPS = [
+    'character_id', 
+    'corporation_id', 
+    'alliance_id', 
+    'faction_id', 
+    'ship_type_id'
+]
+
+WEBHOOK_URL = 'https://discordapp.com/api/webhooks/519251066089373717/MOhnV35wtgIQv8nMy_bD5Eda5EVxcjdm6y3ZmpfFH8nV97i45T_g-xDuoRRo13i-KwIO'  # noqa
+
+# delay in seconds between every message sent to Discord
+# this needs to be >= 1 to prevent 429 Too Many Request errors
+DISCORD_SEND_DELAY = 1
+
+
+class EveEntityQuerySet(models.QuerySet):
+
+    def id2name(self, id: int) -> str:
+        """returns the name for the given ID from the queryset if found
+        
+        else returns ''
+        """
+        try:
+            entity = self.get(id=id)
+        except self.model.DoesNotExist:
+            return ''
+        else:
+            return entity.name
+
 
 class EveEntityManager(models.Manager):
+
+    def get_queryset(self):
+        return EveEntityQuerySet(self.model, using=self._db)
 
     @staticmethod
     def _get_client():
@@ -17,8 +58,7 @@ class EveEntityManager(models.Manager):
             _client = esi_client_factory()
         return _client
     
-    def fetch_entities(self, ids: set):
-        
+    def fetch_entities(self, ids: set) -> list:        
         ids = {int(x) for x in ids}
         ids_found = self.filter(id__in=ids).values_list(flat=True)
         if ids_found:
@@ -29,8 +69,10 @@ class EveEntityManager(models.Manager):
         ids_not_found = ids - ids_found
         if ids_not_found:
             client = self._get_client()
+            logger.info('Fetching ids:\n%s', ids_found)
             results = \
-                client.Universe.post_universe_names(ids=list(ids_not_found)).result()
+                client.Universe.post_universe_names(ids=list(ids_not_found))\
+                .result(timeout=ESI_API_TIMEOUT)
             for item in results:
                 self.update_or_create(
                     id=item['id'],
@@ -40,3 +82,120 @@ class EveEntityManager(models.Manager):
                     }
                 )
         return self.filter(id__in=ids)
+
+    def fetch_entity(self, id: str) -> object:
+        try:
+            entity = self.get(id=id)
+        except self.model.DoesNotExist:
+            return self.fetch_entities([id]).first()
+        else:
+            return entity
+
+
+class KillmailManager(models.Manager):
+    
+    def fetch_killmails(self):
+        from .models import (            
+            KillmailAttacker, KillmailPosition, KillmailVictim, KillmailZkb,     
+        )
+        logger.info('Starting to fetch killmail from ZKB')
+        killmail_counter = 0    
+        for _ in range(10):
+            r = requests.get(ZKB_REDISQ_URL, timeout=ZKB_REDISQ_TIMEOUT)
+            r.raise_for_status()
+            data = r.json()    
+            if data:
+                logger.debug('data:\n%s', data)
+            if data and 'package' in data and data['package']:
+                logger.info('Received a killmail from ZKB')
+                killmail_counter += 1            
+                package_data = data['package']        
+                with transaction.atomic():
+                    killmail_id = int(package_data['killID'])
+                    self.filter(id=killmail_id).delete()
+                    args = {'id': killmail_id}
+                    if 'killmail' in package_data:
+                        killmail_data = package_data['killmail']
+
+                        if 'killmail_time' in killmail_data:
+                            args['time'] = killmail_data['killmail_time']
+
+                        if 'solar_system_id' in killmail_data:
+                            args['solar_system_id'] = killmail_data['solar_system_id']
+
+                    killmail = self.create(**args)
+                    
+                    if 'zkb' in package_data:
+                        zkb_data = package_data['zkb']
+                        args = {'killmail': killmail}
+                        for prop, mapping in (
+                            ('locationID', 'location_id'),
+                            ('hash', None),
+                            ('fittedValue', 'fitted_value'),
+                            ('totalValue', 'total_value'),
+                            ('points', None),
+                            ('npc', 'is_npc'),
+                            ('solo', 'is_solo'),
+                            ('awox', 'is_awox'),
+                        ):
+                            if prop in zkb_data:                            
+                                if mapping:
+                                    args[mapping] = zkb_data[prop]
+                                else:
+                                    args[prop] = zkb_data[prop]
+
+                        KillmailZkb.objects.create(**args)
+                    
+                    if 'killmail' in package_data:
+                        killmail_data = package_data['killmail']
+                        if 'victim' in killmail_data:
+                            victim_data = killmail_data['victim']
+                            args = {'killmail': killmail}
+                            for prop in CHARACTER_PROPS + [
+                                'damage_taken'
+                            ]:
+                                if prop in victim_data:
+                                    args[prop] = victim_data[prop]
+
+                            KillmailVictim.objects.create(**args)
+                            
+                            if 'position' in victim_data:
+                                position_data = victim_data['position']
+                                args = {'killmail': killmail}
+                                for prop in ['x', 'y', 'z']:
+                                    if prop in position_data:
+                                        args[prop] = position_data[prop]
+
+                                KillmailPosition.objects.create(**args)
+                            
+                        if 'attackers' in killmail_data:
+                            for attacker_data in killmail_data['attackers']:
+                                args = {'killmail': killmail}
+                                for prop in CHARACTER_PROPS + [
+                                    'damage_done',                                 
+                                    'security_status', 
+                                    'faction_id', 
+                                    'weapon_type_id'
+                                ]:
+                                    if prop in attacker_data:
+                                        args[prop] = attacker_data[prop]
+
+                                if 'final_blow' in attacker_data:
+                                    args['is_final_blow'] = attacker_data['final_blow']
+
+                                KillmailAttacker.objects.create(**args)
+                
+            else:
+                break
+
+        logger.info('Retrieved %s killmail from ZKB', killmail_counter)
+            
+    def process_killmails(self):                       
+        killmails = self.filter(is_processed=False).select_related()
+        killmail_counter = 0
+        for killmail in killmails:
+            killmail.send_to_webhook(WEBHOOK_URL)
+            sleep(DISCORD_SEND_DELAY)
+            killmail_counter += 1
+            if killmail_counter > 10:
+                break
--- killtracker/models.py
+++ killtracker/models.py
@@ -1,6 +1,14 @@
+import logging
+import urllib
+
+from dhooks_lite import Webhook, Embed, Thumbnail, Footer
+
 from django.db import models
 
-from .managers import EveEntityManager
+from .managers import EveEntityManager, KillmailManager
+
+logger = logging.getLogger(__name__)
+WEBHOOK_URL = 'https://discordapp.com/api/webhooks/519251066089373717/MOhnV35wtgIQv8nMy_bD5Eda5EVxcjdm6y3ZmpfFH8nV97i45T_g-xDuoRRo13i-KwIO'  # noqa
 
 
 class General(models.Model):
@@ -14,18 +22,168 @@ class General(models.Model):
         )
 
 
+class EveEntity(models.Model):
+    
+    ZKB_ENTITY_URL_BASE = 'https://zkillboard.com/'
+
+    CATEGORY_ALLIANCE = 'alliance'
+    CATEGORY_CHARACTER = 'character'
+    CATEGORY_CONSTELLATION = 'constellation'
+    CATEGORY_CORPORATION = 'corporation'
+    CATEGORY_FACTION = 'faction'
+    CATEGORY_INVENTORY_TYPE = 'inventory_type'
+    CATEGORY_REGION = 'region'    
+    CATEGORY_SOLAR_SYSTEM = 'solar_system'
+    CATEGORY_STATION = 'station'    
+
+    CATEGORY_CHOICES = (
+        (CATEGORY_ALLIANCE, 'alliance'),
+        (CATEGORY_CHARACTER, 'character'),
+        (CATEGORY_CONSTELLATION, 'constellation'),
+        (CATEGORY_CORPORATION, 'corporation'),
+        (CATEGORY_FACTION, 'faction'),
+        (CATEGORY_INVENTORY_TYPE, 'inventory_type'),
+        (CATEGORY_REGION, 'region'),
+        (CATEGORY_SOLAR_SYSTEM, 'solar_system'),
+        (CATEGORY_STATION, 'station'),        
+    )
+    
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES)
+    timestamp = models.DateTimeField(auto_now=True, db_index=True)
+
+    objects = EveEntityManager()
+
+    def __repr__(self):
+        return f'{type(self).__name__}(id=\'{self.id}\')'
+
+    @property
+    def zkb_url(self) -> str:
+        map_category_2_path = {
+            self.CATEGORY_ALLIANCE: 'alliance',
+            self.CATEGORY_CHARACTER: 'character',
+            self.CATEGORY_CORPORATION: 'corporation',            
+            self.CATEGORY_INVENTORY_TYPE: 'system',
+            self.CATEGORY_REGION: 'region',
+            self.CATEGORY_SOLAR_SYSTEM: 'system',
+        }
+        if self.category in map_category_2_path:
+            path = map_category_2_path[self.category]
+            return f'{self.ZKB_ENTITY_URL_BASE}{path}/{self.id}/'
+        else:
+            return ''
+
+    @property
+    def zkb_link(self) -> str:
+        url = self.zkb_url
+        if url:
+            return f'[{self.name}]({url})'
+        else:
+            return f'{self.name}'
+
+
 class Killmail(models.Model):
+
+    EVE_IMAGESERVER_BASE_URL = 'https://images.evetech.net'
+    ZKB_KILLMAIL_BASEURL = 'https://zkillboard.com/kill/'
 
     id = models.BigIntegerField(primary_key=True)
     time = models.DateTimeField(default=None, null=True, blank=True)
     solar_system_id = models.PositiveIntegerField(default=None, null=True, blank=True)
     is_processed = models.BooleanField(default=False)
 
+    objects = KillmailManager()
+
     def __str__(self):
         return str(id)
 
     def __repr__(self):
         return f'Killmail(id={self.id})'
+
+    @property
+    def solar_system_name(self):
+        return EveEntity.objects.fetch_entity(self.solar_system_id).name
+
+    @property
+    def solar_system_link(self):
+        return EveEntity.objects.fetch_entity(self.solar_system_id).zkb_link
+
+    def fetch_entities(self):
+        ids = [
+            x for x in [
+                self.victim.character_id,
+                self.victim.corporation_id,
+                self.victim.alliance_id,
+                self.victim.ship_type_id,
+                self.solar_system_id
+            ]
+            if x is not None
+        ]
+        for attacker in self.attackers.all():
+            ids += [
+                x for x in [
+                    attacker.character_id,
+                    attacker.corporation_id,
+                    attacker.alliance_id,
+                    attacker.ship_type_id,
+                    attacker.weapon_type_id,
+                ]
+                if x is not None        
+            ]
+        return EveEntity.objects.fetch_entities(ids)
+
+    def send_to_webhook(self, webhook_url: str = WEBHOOK_URL):
+        self.fetch_entities()
+        try:                        
+            zkb_killmail_url = f'{self.ZKB_KILLMAIL_BASEURL}{self.id}/'            
+            value_mio = int(self.zkb.total_value / 1000000)
+            attacker = self.attackers.get(is_final_blow=True)
+            description = (
+                f'{self.victim.character_link} ({self.victim.corporation_link}) '
+                f'lost their '
+                f'**{self.victim.ship_type_name}** '
+                f'in {self.solar_system_link} worth **{value_mio} M** ISK.\n'
+                f'Final blow by {attacker.character_link} '
+                f'({attacker.corporation_link}) '
+                f'in a **{attacker.ship_type_name}**.'
+            )
+            title = \
+                f'{self.solar_system_name} | {self.victim.character_name} | Killmail'
+            thumbnail_url = self.type_icon_url(self.victim.ship_type_id)
+            footer_text = 'zKillboard'
+            embed = Embed(
+                description=description,
+                title=title,
+                url=zkb_killmail_url,
+                thumbnail=Thumbnail(url=thumbnail_url),
+                footer=Footer(text=footer_text),
+                timestamp=self.time
+            )            
+            logger.info('Sending self to Discord')
+            hook = Webhook(url=webhook_url, username='killtracker')
+            hook.execute(embeds=[embed], wait_for_response=True)            
+            self.is_processed = True
+            self.save()
+           
+        except Exception as ex:
+            logger.exception(ex)
+            pass
+
+    @classmethod
+    def type_icon_url(cls, type_id: int, size: int = 64) -> str:
+        if size < 32 or size > 1024 or (size % 2 != 0):
+            raise ValueError("Invalid size: {}".format(size))
+
+        url = '{}/types/{}/icon'.format(
+            cls.EVE_IMAGESERVER_BASE_URL,
+            int(type_id)
+        )
+        if size:
+            args = {'size': int(size)}
+            url += '?{}'.format(urllib.parse.urlencode(args))
+
+        return url
     
 
 class KillmailCharacter(models.Model):
@@ -39,7 +197,39 @@ class KillmailCharacter(models.Model):
     class Meta:
         abstract = True
 
+    @property
+    def character_name(self):
+        return EveEntity.objects.fetch_entity(self.character_id).name
 
+    @property
+    def character_link(self):
+        return EveEntity.objects.fetch_entity(self.character_id).zkb_link
+
+    @property
+    def corporation_name(self):
+        return EveEntity.objects.fetch_entity(self.corporation_id).name
+
+    @property
+    def corporation_link(self):
+        return EveEntity.objects.fetch_entity(self.corporation_id).zkb_link
+
+    @property
+    def alliance_name(self):
+        return EveEntity.objects.fetch_entity(self.alliance_id).name
+
+    @property
+    def alliance_link(self):
+        return EveEntity.objects.fetch_entity(self.alliance_id).zkb_link
+
+    @property
+    def faction_name(self):
+        return EveEntity.objects.fetch_entity(self.faction_id).name
+
+    @property
+    def ship_type_name(self):
+        return EveEntity.objects.fetch_entity(self.ship_type_id).name
+    
+   
 class KillmailVictim(KillmailCharacter):
 
     killmail = models.OneToOneField(
@@ -51,12 +241,16 @@ class KillmailVictim(KillmailCharacter):
 class KillmailAttacker(KillmailCharacter):
 
     killmail = models.ForeignKey(
-        Killmail, on_delete=models.CASCADE, related_name='attacker'
+        Killmail, on_delete=models.CASCADE, related_name='attackers'
     )
     damage_done = models.BigIntegerField(default=None, null=True, blank=True)
     is_final_blow = models.BooleanField(default=None, null=True, blank=True)
     security_status = models.FloatField(default=None, null=True, blank=True)
     weapon_type_id = models.PositiveIntegerField(default=None, null=True, blank=True)
+
+    @property
+    def weapon_type_name(self):
+        return EveEntity.objects.fetch_entity(self.weapon_type_id).name
 
 
 class KillmailPosition(models.Model):
@@ -81,38 +275,3 @@ class KillmailZkb(models.Model):
     is_npc = models.BooleanField(default=None, null=True, blank=True)
     is_solo = models.BooleanField(default=None, null=True, blank=True)
     is_awox = models.BooleanField(default=None, null=True, blank=True)
-
-
-class EveEntity(models.Model):
-    
-    CATEGORY_ALLIANCE = 'alliance'
-    CATEGORY_CHARACTER = 'character'
-    CATEGORY_CONSTELLATION = 'constellation'
-    CATEGORY_CORPORATION = 'corporation'
-    CATEGORY_FACTIONS = 'factions'
-    CATEGORY_REGIONS = 'regions'    
-    CATEGORY_SOLAR_SYSTEM = 'solar_system'
-    CATEGORY_STATION = 'station'
-    CATEGORY_INVENTORY_TYPE = 'inventory_type'
-
-    CATEGORY_CHOICES = (
-        (CATEGORY_ALLIANCE, 'alliance'),
-        (CATEGORY_CHARACTER, 'character'),
-        (CATEGORY_CONSTELLATION, 'constellation'),
-        (CATEGORY_CORPORATION, 'corporation'),
-        (CATEGORY_FACTIONS, 'factions'),
-        (CATEGORY_REGIONS, 'regions'),
-        (CATEGORY_SOLAR_SYSTEM, 'solar_system'),
-        (CATEGORY_STATION, 'station'),
-        (CATEGORY_INVENTORY_TYPE, 'inventory_type'),
-    )
-    
-    id = models.PositiveIntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES)
-    timestamp = models.DateField(auto_now=True, db_index=True)
-
-    objects = EveEntityManager()
-
-    def __repr__(self):
-        return f'{type(self).__name__}(id=\'{self.id}\')'
