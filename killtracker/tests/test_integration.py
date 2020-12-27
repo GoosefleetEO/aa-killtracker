cea29625f5f0bde041302c3c1e3dac118dcfcc87
ErikKalkoken <erik.kalkoken@gmail.com>
2020-12-27 16:38:49 +0100

    Enhance integration tests

--- killtracker/tests/test_integration.py
+++ killtracker/tests/test_integration.py
@@ -8,7 +8,7 @@ from django.test.utils import override_settings
 
 from . import ResponseStub
 from ..models import Tracker
-from ..tasks import run_killtracker
+from .. import tasks
 from .testdata.helpers import killmails_data, LoadTestDataMixin
 
 PACKAGE_PATH = "killtracker"
@@ -24,8 +24,7 @@ class TestIntegration(LoadTestDataMixin, TestCase):
         super().setUpClass()
         cache.clear()
         cls.tracker_1 = Tracker.objects.create(
-            name="Low Sec Only",
-            exclude_high_sec=True,
+            name="My Tracker",
             exclude_null_sec=True,
             exclude_w_space=True,
             webhook=cls.webhook_1,
@@ -44,7 +43,13 @@ class TestIntegration(LoadTestDataMixin, TestCase):
         mock_execute.return_value = dhooks_lite.WebhookResponse(dict(), status_code=200)
         mock_requests.get.side_effect = self.my_redisq()
 
-        run_killtracker()
-        self.assertTrue(mock_execute.called, True)
-        _, kwargs = mock_execute.call_args
-        self.assertIn("Low Sec Only", kwargs["content"])
+        tasks.run_killtracker.delay()
+        self.assertTrue(mock_execute.call_count, 2)
+
+        _, kwargs = mock_execute.call_args_list[0]
+        self.assertIn("My Tracker", kwargs["content"])
+        self.assertIn("10000001", kwargs["embeds"][0].url)
+
+        _, kwargs = mock_execute.call_args_list[1]
+        self.assertIn("My Tracker", kwargs["content"])
+        self.assertIn("10000002", kwargs["embeds"][0].url)
